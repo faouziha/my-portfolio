a71b58b7c8392621b9ee2e2b79bf61583ffffb29
faouziha <fhaourigui1@gmail.com>
2025-01-04 21:53:24 +0100

    v4

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,46 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes, faFile } from '@fortawesome/free-solid-svg-icons';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import './navbar.css';
 
-export default function Navbar({ onMenuToggle }) {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
-
-    const handleLinkClick = () => {
-        setIsOpen(false);
-    };
-
-    useEffect(() => {
-        onMenuToggle(isOpen);
-    }, [isOpen, onMenuToggle]);
-
-    if (isOpen) {
-        document.body.style.overflow = 'hidden';
-    } else {
-        document.body.style.overflow = 'auto';
-    }
-
+export default function Navbar() {
     return (
         <div className='navbar-container'>
             <div className='navbar'>
-                <a href='#home' className='anchorname' onClick={handleLinkClick}><h2 className='name'>Faouzi haourigui</h2></a>
-                <div className='burger' onClick={toggleMenu}>
-                    <FontAwesomeIcon icon={isOpen ? faTimes : faBars} size='lg' />
-                </div>
-                <div className={`menu ${isOpen ? 'open' : ''}`}>
-                    <a href='#home' className='navanchor home' onClick={handleLinkClick}><h4>Home</h4></a>
-                    <a href='#about' className='navanchor about' onClick={handleLinkClick}><h4>About</h4></a>
-                    <a href='#projects' className='navanchor projects' onClick={handleLinkClick}><h4>Projects</h4></a>
-                    <a href='#skills' className='navanchor projects' onClick={handleLinkClick}><h4>Skills</h4></a>
-                    <a href='#contact' className='navanchor contact' onClick={handleLinkClick}><h4>Contact</h4></a>
+                <a href='#home' className='anchorname'><h2 className='name'>Faouzi haourigui</h2></a>
+                <div className='menu'>
+                    <a href='#home' className='navanchor home'><h4>Home</h4></a>
+                    <a href='#about' className='navanchor about'><h4>About</h4></a>
+                    <a href='#projects' className='navanchor projects'><h4>Projects</h4></a>
+                    <a href='#skills' className='navanchor projects'><h4>Skills</h4></a>
+                    <a href='#contact' className='navanchor contact'><h4>Contact</h4></a>
                     <div className='icons'>
-                        <a className='icon' href="/resume/FaouziHaouriguiResume.pdf" download target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFile} /></a>
-                        <a className='icon' href="https://www.linkedin.com/in/faouzi-haourigui-4b2517275/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin}  /></a>
+                        <a className='icon' href="/resume/FaouziHaouriguiResume (1).pdf" download target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFile} /></a>
+                        <a className='icon' href="https://www.linkedin.com/in/faouzi-haourigui-4b2517275/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
                         <a className='icon' href="https://www.instagram.com/faouzihaourigui/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} /></a>
                     </div>
                 </div>
